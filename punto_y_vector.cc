f355ae1fce509b88b17a79c817b30be7777792e5
4444borja <741440@unizar.es>
2020-09-29 18:44:11 +0200

    Operaciones +-*/

---
+++ punto_y_vector.cc
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+struct Punto_Vector
+{
+    double x, y, z;
+    int valor;
+
+    Punto_Vector (double x, double y, double z, int valor) {
+        if (valor == 0 || valor == 1) {
+            this -> x = x;
+            this -> y = y;
+            this -> z = z;
+            this -> valor = valor;
+        }
+        else{
+            cerr << "El cuarto argumento solo admite 0 (direcciones) y 1 (puntos)" << endl;
+            exit(1);
+        }
+    }
+};
+
+Punto_Vector operator+(const Punto_Vector &p1, const Punto_Vector &p2)
+{
+    if (p1.valor == 1 && p2.valor == 1){
+        // si ambos son puntos
+        cerr << "Sumar dos puntos no tiene sentido geometrico" << endl;
+        exit(1);
+    }
+    int valor = 1;
+    if (p1.valor == 0 && p2.valor == 0) {
+        // si ambos son direcciones
+        valor = 0;
+    }
+    Punto_Vector p(p1.x + p2.x, p1.y + p2.y, p1.z + p2.z, valor);
+    return p;
+}
+
+Punto_Vector operator-(const Punto_Vector &p1, const Punto_Vector &p2)
+{
+    if (p1.valor == 1 && p2.valor == 1){
+        // si ambos son puntos
+        cerr << "Restar dos puntos no tiene sentido geometrico" << endl;
+        exit(1);
+    }
+    int valor = 1;
+    if (p1.valor == 1 && p2.valor == 0) {
+        // p1 punto y p2 direccion
+        cerr << "No se puede restar un punto menos una direccion" << endl;
+        exit(1);
+    }
+    Punto_Vector p(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z, 0);
+    return p;
+}
+
+Punto_Vector operator*(const Punto_Vector &p1, const double &s)
+{
+    if (p1.valor == 1){
+        // si es punto
+        cerr << "No se puede multiplicar un punto por un escalar" << endl;
+        exit(1);
+    }
+    Punto_Vector p(p1.x * s, p1.y * s, p1.z * s, 1);
+    return p;
+}
+
+Punto_Vector operator/(const Punto_Vector &p1, const double &s)
+{
+    if (p1.valor == 0){
+        // si es direccion
+        cerr << "No se puede dividir una direccion por un escalar" << endl;
+        exit(1);
+    }
+    Punto_Vector p(p1.x * s, p1.y * s, p1.z * s, 1);
+    return p;
+}
+
+int main() {
+    Punto_Vector A(2,3,4,1);
+    Punto_Vector B(1,2,3,0);
+
+    Punto_Vector C = A+B;
+    Punto_Vector D = A*5;
+
+    cout << A.x << " " << A.y << " " << A.z << " " << A.valor << endl;
+    cout << B.x << " " << B.y << " " << B.z << " " << B.valor << endl;
+    cout << C.x << " " << C.y << " " << C.z << " " << C.valor << endl;
+    cout << D.x << " " << D.y << " " << D.z << " " << D.valor << endl;
+
+    return 0;
+}
