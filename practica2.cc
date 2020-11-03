30b429ba1ec305f9b546b40090dc58038c573c2e
Alejandro Facorro <Greyscarr@users.noreply.github.com>
2020-11-03 22:35:40 +0100

    Hecho calculos de entrada y crea un fichero al sacar los datos

--- practica2.cc
+++ practica2.cc
@@ -19,12 +19,19 @@ int main() {
     string formato;
     int width, height;
     int res_color;
+    float MAX = 1;
 
     int i = 0;
     while(!fichero.eof()) {
         getline(fichero,cadena);
 
-        if (cadena.at(0)=='#') continue;
+        if (cadena.at(0)=='#'){
+            if(cadena.substr(1,3) == "MAX"){
+                string aux = cadena.substr(5,cadena.length());
+                MAX = stof(aux);
+            }
+            continue;
+        }
 
         if (i == 0) {
             formato = cadena;
@@ -47,17 +54,37 @@ int main() {
     cout << height << endl;
     cout << res_color << endl;
 
-    vector<int> imagen (height*width*3);
+    vector<float> imagen (height*width*3);
 
     for (int i = 0; i < height; i++) {
         getline(fichero,cadena);
         //cout << cadena << endl;
         std::istringstream iss(cadena);
+        float auxiliar_s;
         for (int j = 0; j < width; j++) {
-            iss >> imagen[i*width*3 + j*3];
-            iss >> imagen[i*width*3 + j*3 + 1];
-            iss >> imagen[i*width*3 + j*3 + 2];
+            iss >> auxiliar_s;
+            imagen[i*width*3 + j*3] = (MAX * auxiliar_s) / res_color;
+            iss >> auxiliar_s;
+            imagen[i*width*3 + j*3 + 1] = (MAX * auxiliar_s) / res_color;
+            iss >> auxiliar_s;
+            imagen[i*width*3 + j*3 + 2] = (MAX * auxiliar_s) / res_color;
         }
+    }
+
+
+    ofstream out;
+    out.open("practica_2.ppm");
+    out << formato << endl;
+    out << width << " " << height << endl;
+    out << res_color << endl;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            
+            out << imagen[i*width*3 + j*3] * res_color / MAX << " " 
+            << imagen[i*width*3 + j*3 + 1] * res_color / MAX << " "
+            << imagen[i*width*3 + j*3 + 2] * res_color / MAX << " ";
+        }
+        out << endl;
     }
 
     fichero.close();
