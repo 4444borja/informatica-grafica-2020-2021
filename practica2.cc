09898e564aefb3f7d48b92559c4201d058fdc6de
4444borja <741440@unizar.es>
2020-11-05 11:48:00 +0100

    eq nd clamping

--- practica2.cc
+++ practica2.cc
@@ -28,7 +28,9 @@ int main() {
         if (cadena.at(0)=='#'){
             if(cadena.substr(1,3) == "MAX"){
                 string aux = cadena.substr(5,cadena.length());
-                MAX = stof(aux);
+                //MAX = stof(aux);
+                std::istringstream iss(aux);
+                iss >> MAX;
             }
             continue;
         }
@@ -49,12 +51,14 @@ int main() {
             break;
         }
     }
-    cout << formato << endl;
+    /*cout << formato << endl;
     cout << width << endl;
     cout << height << endl;
     cout << res_color << endl;
+    cout << MAX << endl;*/
 
     vector<float> imagen (height*width*3);
+    float max = 0, min = res_color;
 
     for (int i = 0; i < height; i++) {
         getline(fichero,cadena);
@@ -64,13 +68,80 @@ int main() {
         for (int j = 0; j < width; j++) {
             iss >> auxiliar_s;
             imagen[i*width*3 + j*3] = (MAX * auxiliar_s) / res_color;
+            if ((MAX * auxiliar_s) / res_color < min) {
+                min = (MAX * auxiliar_s) / res_color;
+            } else if ((MAX * auxiliar_s) / res_color > max){
+                max = (MAX * auxiliar_s) / res_color;
+            }
             iss >> auxiliar_s;
             imagen[i*width*3 + j*3 + 1] = (MAX * auxiliar_s) / res_color;
+            if ((MAX * auxiliar_s) / res_color < min) {
+                min = (MAX * auxiliar_s) / res_color;
+            } else if ((MAX * auxiliar_s) / res_color > max){
+                max = (MAX * auxiliar_s) / res_color;
+            }
             iss >> auxiliar_s;
             imagen[i*width*3 + j*3 + 2] = (MAX * auxiliar_s) / res_color;
+            if ((MAX * auxiliar_s) / res_color < min) {
+                min = (MAX * auxiliar_s) / res_color;
+            } else if ((MAX * auxiliar_s) / res_color > max){
+                max = (MAX * auxiliar_s) / res_color;
+            }
         }
     }
 
+    // TODO: tone mapping //////////////////
+
+    // clamping
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (imagen[i*width*3 + j*3] > 1) {
+                imagen[i*width*3 + j*3] = 1;
+            }
+            if (imagen[i*width*3 + j*3 + 1] > 1) {
+                imagen[i*width*3 + j*3 + 1] = 1;
+            }
+            if (imagen[i*width*3 + j*3 + 2] > 1) {
+                imagen[i*width*3 + j*3 + 2] = 1;
+            }
+        }
+    }
+
+    //eq
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            imagen[i*width*3 + j*3] = imagen[i*width*3 + j*3] / max * 1;
+            imagen[i*width*3 + j*3 + 1] = imagen[i*width*3 + j*3 + 1] / max * 1;
+            imagen[i*width*3 + j*3 + 2] = imagen[i*width*3 + j*3 + 2] / max * 1;
+        }
+    }
+
+    //eq and clamp
+    float V = 1;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (imagen[i*width*3 + j*3] > V) {
+                imagen[i*width*3 + j*3] = V;
+            } else {
+                imagen[i*width*3 + j*3] = imagen[i*width*3 + j*3] / V * 1;
+            }
+            if (imagen[i*width*3 + j*3 + 1] > V) {
+                imagen[i*width*3 + j*3 + 1] = V;
+            } else {
+                imagen[i*width*3 + j*3 + 1] = imagen[i*width*3 + j*3 + 1] / V * 1;
+            }
+            if (imagen[i*width*3 + j*3 + 2] > V) {
+                imagen[i*width*3 + j*3 + 2] = V;
+            } else {
+                imagen[i*width*3 + j*3 + 2] = imagen[i*width*3 + j*3 + 2] / V * 1;
+            }
+        }
+    }
+
+
+
+
+    ////////////////////////////////////////
 
     ofstream out;
     out.open("practica_2.ppm");
