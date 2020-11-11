1acef2bf1f5e2a0db9ade832a6a02fb451a1788b
4444borja <741440@unizar.es>
2020-11-11 20:25:26 +0100

    multithreading funciona

--- practica3.cc
+++ practica3.cc
@@ -7,6 +7,7 @@
 #include <stdlib.h> 
 #include <vector>
 #include <chrono>
+#include <thread>
 #include "punto_y_vector.cc"
 
 using namespace std;
@@ -55,6 +56,53 @@ class Camera{
 
 };
 
+void rellenar_imagen_esfera(vector<float> &imagen, const int resolution, Punto_Vector centro_esfera, double radio_esfera, Camera cam, int thread){
+    int j1, j2;
+    j1 = (thread - 1) * (resolution / 8);
+    j2 = thread * (resolution / 8);
+
+    if (thread < 1 || thread > 8) {
+        std::cout << "Error en los threads" << endl;
+        exit(1);
+    }
+
+    for (int i = resolution-1; i >= 0; i--){
+        for (int j = j1; j < j2; j++){
+            // para cada pixel
+            Ray r = cam.get_center_ray(i,j,resolution);
+
+            Punto_Vector origen_rayo = r.origen;
+            Punto_Vector dir_rayo = r.direccion;
+            
+            // Esfera
+            double a = pow(dir_rayo.modulo(), 2);
+            double b = dir_rayo ^ (centro_esfera - origen_rayo) * 2;
+            double c = pow((centro_esfera - origen_rayo).modulo(), 2) - pow(radio_esfera, 2);
+            //cout << "VALORes: " << a << " " << b<< " "  << c << endl;
+            if ((b*b - 4*a*c) < 0) {
+                // si es negativo no ha dado a nada, escribimos negro
+                imagen[i*resolution*3 + j*3] = 0;
+                imagen[i*resolution*3 + j*3 + 1] = 0;
+                imagen[i*resolution*3 + j*3 + 2] = 0;
+            }
+            else if((b*b - 4*a*c) == 0) {
+                // si es tangente (muy improbable)
+                imagen[i*resolution*3 + j*3] = 0;
+                imagen[i*resolution*3 + j*3 + 1] = 255;
+                imagen[i*resolution*3 + j*3 + 2] = 0;
+                cout << "TANGENTE" << endl;
+            }
+            else {
+                // ha dado a la esfera
+                imagen[i*resolution*3 + j*3] = 255;
+                imagen[i*resolution*3 + j*3 + 1] = 255;
+                imagen[i*resolution*3 + j*3 + 2] = 0;
+            }
+            
+        }
+    }
+}
+
 
 int main(int argc, char **argv) {
     int number_of_rays = 1;
@@ -89,7 +137,7 @@ int main(int argc, char **argv) {
 
     chrono::steady_clock::time_point begin = chrono::steady_clock::now();
 
-    for (int i = resolution-1; i >= 0; i--){
+    /*for (int i = resolution-1; i >= 0; i--){
         for (int j = 0; j < resolution; j++){
             // para cada pixel
             for(int k = 0; k < number_of_rays; k++) {
@@ -118,7 +166,7 @@ int main(int argc, char **argv) {
                 }
 
                 // Plano 
-                /*double paralelo = dir_rayo ^ normal_plano_limit;
+                double paralelo = dir_rayo ^ normal_plano_limit;
                 //cout << paralelo << endl;
                 if(paralelo == 0){
                     // El plano es paralelo, mostramos negro
@@ -143,12 +191,32 @@ int main(int argc, char **argv) {
                             out << "0 0 0 ";
                         }
                     }
-                }*/
+                }
 
             }
         }
         out << endl;
+    }*/
+
+    // generar threads
+    std::thread threads[8];
+    for (int t = 1; t <= 8; t++) {
+        threads[t-1] = std::thread(rellenar_imagen_esfera, std::ref(imagen), resolution, centro_esfera, radio_esfera, cam, t);
     }
+    for (int t = 1; t <= 8; t++) {
+        threads[t-1].join();
+    }
+
+    // volcarlo al fichero
+    for (int i = 0; i < resolution; i++) {
+        for (int j = 0; j < resolution; j++) {
+            for (int k = 0 ; k < 3 ; k++){
+                out << (int)(imagen[i*resolution*3 + j*3 + k] * 255 / 255) << " " ;
+            }
+        }
+        out << endl;
+    }
+
     chrono::steady_clock::time_point end = chrono::steady_clock::now();
 
     cout << "Tiempo: " << (chrono::duration_cast<std::chrono::microseconds>(end - begin).count())/1000000.0 << " segundos" << endl;
