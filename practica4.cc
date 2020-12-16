c809576255a05b65a8c79c1a3ebbbb0bb4899e17
4444borja <borjaaguadodiez@gmail.com>
2020-12-16 15:28:25 +0100

    rgb y practica4

--- practica4.cc
+++ practica4.cc
@@ -58,6 +58,8 @@ class Camera{
 
 };
 
+
+
 void rellenar_imagen_esfera(vector<float> &imagen, const int resolution, esfera escena[], Camera cam, int thread, int rebotes){
     int j1, j2;
     j1 = (thread - 1) * (resolution / 8);
@@ -69,10 +71,11 @@ void rellenar_imagen_esfera(vector<float> &imagen, const int resolution, esfera 
     }
     for (int i = resolution-1; i >= 0; i--){
         for (int j = j1; j < j2; j++){
+            // para cada pixel
             imagen[i*resolution*3 + j*3] = 0;
             imagen[i*resolution*3 + j*3 + 1] = 0;
             imagen[i*resolution*3 + j*3 + 2] = 0;
-            // para cada pixel
+            
             Ray r = cam.get_center_ray(i,j,resolution);
             double radiancia_total = 0;
             for(int k = 0; k < rebotes ; k++){
@@ -111,41 +114,29 @@ void rellenar_imagen_esfera(vector<float> &imagen, const int resolution, esfera 
                     // Preguntar si el sistema de coordenadas tiene un origen de 0,0,0 o el punto de origen de la figura
                     Matrix matrizLocal(vector_x,vector_y,vector_z,punto_figura);
 
-                    // Usando las coordenadas locales, creamos dos num aleatorio entre [-90,90] grados
-                    // Realizamos una inclinación y una azimuth, como en la practica 1, obtenemos una direccion en coordenadas locales
-                    int azimuth = rand() % 181 - 90;
-                    int inclination = rand() % 181 - 90;
+                    // PARA MATERIAL DIFUSO
+                    float random1 = (float) rand()/RAND_MAX;
+                    float random2 = (float) rand()/RAND_MAX;
 
-                    
-                    double aux_vectorIncl_1[4] = {cos(inclination),0,-sin(inclination),0};
-                    double aux_vectorIncl_2[4] = {0,1,0,0};
-                    double aux_vectorIncl_3[4] = {sin(inclination),0,cos(inclination),0};
-                    double aux_vectorIncl_4[4] = {0,0,0,1};
-                    Matrix aux_matrixIncl(aux_vectorIncl_1,aux_vectorIncl_2,aux_vectorIncl_3,aux_vectorIncl_4);
-                    // Realizamos en giro conforme al ángulo X, que es el vector que va desde el centro del planeta hasta el limite del mismo y es perpendicular
-                    // al vector Y
-                    double vectorAzim_1[4] = {1,0,0,0};
-                    double vectorAzim_2[4] = {0,cos(azimuth),sin(azimuth),0};
-                    double vectorAzim_3[4] = {0,-sin(azimuth),cos(azimuth),0};
-                    double vectorAzim_4[4] = {0,0,0,1};
-                    Matrix matrixAzim(vectorAzim_1,vectorAzim_2,vectorAzim_3,vectorAzim_4);
-                    // Creamos una direccion que sea el vector y y calculamos la nueva direccion con giros, primero en base a Inclinacion y después en base a Azimuth
-                    Punto_Vector dir_local = vector_y;
-                    dir_local = matrixIncl*dir_local;
-                    dir_local = matrixAzim*dir_local;
+                    double zeta = acos(sqrt(1 - (double) random1));
+                    double fi = 2 * PI * (double) random2;
 
+                    Punto_Vector direc_local = Punto_Vector(sin(zeta) * cos(fi),
+                                                                sin(zeta) * sin(fi),
+                                                                cos(zeta),
+                                                                0);
                     // Transformamos de coordenadas locales a globales, con la inversa y con eso tenemos el punto y direccion
+                    Matrix inversaBaseM = InverseOfMatrix(matrizLocal,4);
+                    Punto_Vector direc_global = inversaBaseM*direc_local;
 
-                    Matrix inversaBase = InverseOfMatrix(matrizLocal,4);
-                    Punto_Vector dir_global = inversaBase*dir_local;
+                    double valor_w_normal = abs(vector_y^direc_global);
+                    r.origen = centro_figura;
+                    r.direccion = direc_global;
+
                     // Sumamos el valor de reflectancia y demas al pixel.
-
                     // fr = kd(objeto) / pi;
                     // p(wi) = probabilidad de seguir la ruleta;
                     //radiancia_total = radiancia_total*valor_w_normal * fr(objeto) / p(wi);
-                    double valor_w_normal = abs(vector_y^dir_global);
-                    r.origen = centro_figura;
-                    r.direccion = dir_global;
                     
                 }
                 //Radiancia al pixel = radiancia_luz * radiancia_total;
@@ -158,12 +149,12 @@ void rellenar_imagen_esfera(vector<float> &imagen, const int resolution, esfera 
 int main(int argc, char **argv) {
     int number_of_rays = 1;
 
-    const int resolution = 2048;
+    const int resolution = 512;
 
     vector<float> imagen (resolution * resolution * 3);
 
     ofstream out;
-    out.open("practica_3.ppm");
+    out.open("practica_4.ppm");
     out << "P3" << endl;
     out << resolution << " " << resolution << endl;
     out << 255 << endl;
@@ -203,66 +194,6 @@ int main(int argc, char **argv) {
     chrono::steady_clock::time_point begin = chrono::steady_clock::now();
 
     int rebotes = 4;
-    /*for (int i = resolution-1; i >= 0; i--){
-        for (int j = 0; j < resolution; j++){
-            // para cada pixel
-            for(int k = 0; k < number_of_rays; k++) {
-                Ray r = cam.get_center_ray(i,j,resolution);
-
-                Punto_Vector origen_rayo = r.origen;
-                Punto_Vector dir_rayo = r.direccion;
-                
-                // Esfera
-                double a = pow(dir_rayo.modulo(), 2);
-                double b = dir_rayo ^ (centro_esfera - origen_rayo) * 2;
-                double c = pow((centro_esfera - origen_rayo).modulo(), 2) - pow(radio_esfera, 2);
-                //cout << "VALORes: " << a << " " << b<< " "  << c << endl;
-                if ((b*b - 4*a*c) < 0) {
-                    // si es negativo no ha dado a nada, escribimos negro
-                    out << "0 0 0 ";
-                }
-                else if((b*b - 4*a*c) == 0) {
-                    // si es tangente (muy improbable)
-                    out << "0 0 255 ";
-                    cout << "TANGENTE" << endl;
-                }
-                else {
-                    // ha dado a la esfera
-                    out << "255 255 0 ";
-                }
-
-                // Plano 
-                double paralelo = dir_rayo ^ normal_plano_limit;
-                //cout << paralelo << endl;
-                if(paralelo == 0){
-                    // El plano es paralelo, mostramos negro
-                    out << "0 0 0 ";
-                }
-                else{
-                    //cout << (-(distancia_origen+(origen_rayo^normal_plano))/paralelo) << endl;
-                    double t = -(distancia_origen_limit+(origen_rayo^normal_plano_limit))/paralelo;
-                    if (t < 0){
-                        // El plano está detrás de la cámara, mostramos negro
-                        out << "0 0 0 ";
-                    }
-                    else{
-                        // Ha dado al plano, mostramos amarillo
-                        double punto_x = origen_rayo.x + t*dir_rayo.x;
-                        double punto_y = origen_rayo.y + t*dir_rayo.y;
-                        double punto_z = origen_rayo.z + t*dir_rayo.z;
-                        if((plano_x*punto_x+plano_y*punto_y+plano_z*punto_z + distancia_origen_limit) == 0){
-                            out << "255 255 0 ";
-                        }
-                        else{
-                            out << "0 0 0 ";
-                        }
-                    }
-                }
-
-            }
-        }
-        out << endl;
-    }*/
 
     // generar threads
     std::thread threads[8];
