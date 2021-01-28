52955bdeffd9bd8de51f5bd6342db1d68dbec3d7
4444borja <borjaaguadodiez@gmail.com>
2021-01-28 18:59:32 +0100

    next event estimation en blanco y negro

--- practica4.cc
+++ practica4.cc
@@ -63,7 +63,7 @@ class Camera{
 
 };
 
-std::tuple<int,int,int> funcionL(vector<Geometria*> escena, Ray r, double n0, double figura_actual){
+std::tuple<int,int,int> funcionL(vector<Geometria*> escena, Ray r, double n0, double figura_actual, bool rayo_origen){
     Punto_Vector origen_rayo = r.origen;
     Punto_Vector dir_rayo = r.direccion;
     // Esfera
@@ -113,42 +113,42 @@ std::tuple<int,int,int> funcionL(vector<Geometria*> escena, Ray r, double n0, do
                 normal = normal.normalizar();
             }
 
+
             //next event estimation
-            //comprobar 
 
-            /*Punto_Vector punto_luz = Punto_Vector(-20,0,20,1);
-            double distancia_a_luz = sqrt( pow(origen_rayo.x - punto_luz.x,2) +
-                                           pow(origen_rayo.y - punto_luz.y,2) +
-                                           pow(origen_rayo.z - punto_luz.z,2) );
-            bool luz_nee = false;
+            Punto_Vector punto_luz = Punto_Vector(-20,0,20,1);
+            double distancia_a_luz = sqrt( pow(punto_figura.x - punto_luz.x,2) +
+                                           pow(punto_figura.y - punto_luz.y,2) +
+                                           pow(punto_figura.z - punto_luz.z,2) );
             
-
-            double t_valor_min_nee = numeric_limits<double>::max();
+            
             Punto_Vector normal_nee;
-            Punto_Vector dir_rayo_nee = punto_luz - origen_rayo;
+            Punto_Vector dir_rayo_nee = punto_luz - punto_figura;
+            dir_rayo_nee = dir_rayo_nee.normalizar();
+
+            punto_figura = punto_figura + normal * 0.02;
+
+            //cout << dir_rayo_nee.x << " " << dir_rayo_nee.y << " " << dir_rayo_nee.z << " " << endl;
 
             for(int i = 0; i < escena.size() ; i++){
-                if (i_figura == i) {
+                /*if (i_figura == i){
                     continue;
-                }
-                double distancia_a_objeto = escena[i]->get_interseccion(origen_rayo,dir_rayo_nee, normal_nee);
+                }*/
+                double distancia_a_objeto = escena[i]->get_interseccion(punto_figura,dir_rayo_nee, normal_nee);
 
-                if((distancia_a_objeto < t_valor_min_nee) && distancia_a_objeto >= 0){
-                    // se ha intersecado con algo
-                    if(distancia_a_objeto > distancia_a_luz) {
-                        //ha intersectado con un objeto que está más allá del punto de luz, por lo que no hay sombra
-                        luz_nee = true;
-                    }
-                    else{
-                        //la distancia al objeto es menor que a la luz, hay que comprobar si el objeto está entre el punto y la luz, o detrás del punto
-                        double distancia_luz_objeto = escena[i]->get_interseccion(punto_luz, dir_rayo_nee, normal_nee);
-                        if(distancia_luz_objeto > distancia_a_luz) {
-                            // si la distancia entre la luz y el objeto es mayor a la distancia entre la uz y el punto, el objeto no tapa la luz -> no hace sombra
-                            luz_nee = true;
-                        }
-                    }
+                if((distancia_a_objeto < distancia_a_luz) && distancia_a_objeto >= 0){
+                    // se hace sombra
+                    return std::make_tuple(0, 0, 0);
+                    //goto Sombra;
                 }
-            }   */
+            }
+            //cout << "luz" << endl;
+            return std::make_tuple(255, 255, 255);
+
+
+            Sombra:
+
+                
 
             double maxpd, maxps, maxpt, pd, ps,pt;
             if(escena[i_figura]->es_dielectrico()){
@@ -196,7 +196,7 @@ std::tuple<int,int,int> funcionL(vector<Geometria*> escena, Ray r, double n0, do
                 r.origen = punto_figura + normal * 0.002;
                 r.direccion = direccion_reflejada.normalizar();
 
-                std::tuple<int, int, int> siguiente = funcionL(escena,r,n0,i_figura);
+                std::tuple<int, int, int> siguiente = funcionL(escena,r,n0,i_figura,false);
 
                 rgb colores_ks = escena[i_figura]->get_colores_ks();
 
@@ -223,7 +223,7 @@ std::tuple<int,int,int> funcionL(vector<Geometria*> escena, Ray r, double n0, do
                 r.origen = punto_figura + cambio * 0.02;
                 r.direccion = direccion_refractada.normalizar();
 
-                std::tuple<int, int, int> siguiente = funcionL(escena,r,n1,i_figura);
+                std::tuple<int, int, int> siguiente = funcionL(escena,r,n1,i_figura,false);
 
                 rgb colores_kt = escena[i_figura]->get_colores_kt();
 
@@ -280,7 +280,7 @@ std::tuple<int,int,int> funcionL(vector<Geometria*> escena, Ray r, double n0, do
 
 
                 // el rayo sigue
-                std::tuple<int, int, int> siguiente = funcionL(escena,r,n0,i_figura);
+                std::tuple<int, int, int> siguiente = funcionL(escena,r,n0,i_figura,false);
                 // pasamos los colores a [0, 1] para multiplicarlos
                 rgb colores_kd = escena[i_figura]->get_colores_kd();
 
@@ -333,7 +333,7 @@ void rellenar_imagen_esfera(vector<float> &imagen, const int resolution,  vector
             std::tuple<int, int, int> totalPixel = std::make_tuple(0, 0, 0);
 
             for (int i = 0; i < number_of_rays; i++){
-                std::tuple<int, int, int> colorPixel = funcionL(escena,r,1,-1);
+                std::tuple<int, int, int> colorPixel = funcionL(escena,r,1,-1,true);
 
                 get<0>(totalPixel) = get<0>(totalPixel) + get<0>(colorPixel);
                 get<1>(totalPixel) = get<1>(totalPixel) + get<1>(colorPixel);
@@ -393,7 +393,9 @@ int main(int argc, char **argv) {
     geo.push_back(new Plano(Punto_Vector(0,0,100,1),Punto_Vector(0,1,0,0),Punto_Vector(1,0,0,0),blanco,nada,nada,0, false,false,0 ));
     geo.push_back(new Plano(Punto_Vector(0,0,-100,1),Punto_Vector(0,1,0,0),Punto_Vector(1,0,0,0),rosa,nada,nada,0, false,false,0 ));
 
-    geo.push_back(new Esfera(Punto_Vector(10,0,15,1), 3 , azul,nada,nada,0, false,false,0));
+    geo.push_back(new Esfera(Punto_Vector(0,0,25,1), 3 , azul,nada,nada,0, false,false,0));
+
+    //geo.push_back(new Esfera(Punto_Vector(10,0,15,1), 3 , azul,nada,nada,0, false,false,0));
     //geo.push_back(new Esfera(Punto_Vector(5,10,16,1), 3, amarillo,blanco,nada,false,0));
     geo.push_back(new Esfera(Punto_Vector(-5,-10,16,1), 3, nada,blanco,nada,0, false,false,0));
     geo.push_back(new Esfera(Punto_Vector(5,10,16,1), 3, nada,nada,blanco,1.45, false,false,0));
